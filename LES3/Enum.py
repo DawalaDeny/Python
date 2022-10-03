bdf3758c7f4b49ca393ba58db00bdaab24410977
DawalaDeny <shabouevdeny@gmail.com>
2022-10-03 12:34:05 +0200

    les3

--- LES3/Enum.py
+++ LES3/Enum.py
@@ -1,4 +1,10 @@
 from enum import Enum
-class AirTyppe(Enum):
+class AirType(Enum):
     jet=0,
     propeller=1
+class GroundType(Enum):
+    wheel = 0,
+    tracks=1
+if __name__ == '__main__':
+    a = AirType.jet
+    print(a)
