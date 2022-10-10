2f3f8b8190292af2fe0b2e33d51e36fda8ed153a
DawalaDeny <shabouevdeny@gmail.com>
2022-10-10 12:59:26 +0200

    les4

--- LES4/gameobjects/Ocean.py
+++ LES4/gameobjects/Ocean.py
@@ -1,15 +1,17 @@
 class Ocean:
     def __init__(self, grooteSpeelveld:int, aantalschepen:int):
-        self.speelveld = list([grooteSpeelveld,grooteSpeelveld])
+        self.speelveld = [[0]*grooteSpeelveld] * grooteSpeelveld
         self.range = grooteSpeelveld
         self.aantalSchepen = aantalschepen
 
     def opvullenSpeelveld(self):
-        for j in range(self.range):
-            print(self.speelveld)
-            rij = [0 for i in range(self.range)]
-            self.speelveld.append(rij)
-        print(self.speelveld)
+        print("---0, 1, 2, 3, 4----")
+        x = 0
+        for i in self.speelveld:
+            print (x, i)
+            while x > self.range:
+                x = +1
+
 
 if __name__ == '__main__':
     ocean1 = Ocean(grooteSpeelveld=5, aantalschepen=1)
