2ad64b676f65642b5365d1dcf4a28f8a151c902d
DawalaDeny <shabouevdeny@gmail.com>
2022-10-11 11:23:41 +0200

    speelveld opvullen gemaakt

--- LES4/gameobjects/Ocean.py
+++ LES4/gameobjects/Ocean.py
@@ -1,17 +1,21 @@
 class Ocean:
     def __init__(self, grooteSpeelveld:int, aantalschepen:int):
-        self.speelveld = [[0]*grooteSpeelveld] * grooteSpeelveld
+        self.speelveld = [["."]*grooteSpeelveld] * grooteSpeelveld
         self.range = grooteSpeelveld
         self.aantalSchepen = aantalschepen
 
     def opvullenSpeelveld(self):
-        print("---0, 1, 2, 3, 4----")
-        x = 0
-        for i in self.speelveld:
-            print (x, i)
-            while x > self.range:
-                x = +1
+        rijI = 0
+        str:str = f"  0 1 2 3 4\n"
+        str += f"{rijI} "
+        for i in range(self.range):
+            for j in range(self.range):
+                str += f"{self.speelveld[i][j]} "
+            rijI += 1
+            if (rijI < self.range):
+                str += f"\n{rijI} "
 
+        print(str)
 
 if __name__ == '__main__':
     ocean1 = Ocean(grooteSpeelveld=5, aantalschepen=1)
