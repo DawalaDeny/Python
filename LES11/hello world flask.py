4b991cd1e486d4dd1352e74ca2d80e50b88e0812
DawalaDeny <shabouevdeny@gmail.com>
2022-12-05 12:41:49 +0100

    les11 - css

--- LES11/hello world flask.py
+++ LES11/hello world flask.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder='templates',static_folder="templates/static")
 
 @app.route("/")
 def index():
