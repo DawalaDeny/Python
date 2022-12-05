c86aabb623f6dc5652bc58664804dc59df43396a
DawalaDeny <shabouevdeny@gmail.com>
2022-12-05 12:26:43 +0100

    les11

---
+++ LES11/hello world flask.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__, template_folder='templates')
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route('/helloworld')
+def hello_world():
+    return 'Hello, World! sldf;dlf;sdfsdlf;sdlf;sdlf;sd'
+
+@app.route('/form')
+def html_form():
+  return """
+  <form method="post">
+  <label for="voornaam">Voornaam:</label>
+  <input type="text" name="voornaam" autofocus><br><br>
+  <label for="familienaam">Familienaam:</label>
+  <input type="text" name="familienaam"><br><br>
+  <button type="submit">Submit</button>
+  </form>
+  """
+
+if __name__ == '__main__':
+ app.run()
